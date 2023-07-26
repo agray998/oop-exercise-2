8e87ddbd2cf214780f6d4684c67ff5d856a7ec74
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2023-07-26 07:22:20 +0100

    added task file

---
+++ clock.py
@@ -0,0 +1,27 @@
+'''
+Task: create a clock class. Each clock should have the attributes hours, minutes and seconds.
+The class should have methods to:
+ - initialise these attributes upon instance creation
+ - update the clock's time by one second when called (remembering to wrap appropriately after every 60 seconds)
+ - return a string representation of the current clock time
+ Create a subclas of this class for 12-hour clocks, and override the methods appropriately
+'''
+class Clock():
+  def __init__(self, hours = 0, minutes = 0, seconds = 0):
+    ...
+
+  def tick(self):
+    ...
+
+  def __str__(self):
+    ...
+
+class TwelveHourClock(Clock):
+  ...
+
+if __name__ == '__main__':
+  clock = Clock(0, 0, 0)
+  while True:
+    clock.tick()
+    print(clock)
+    sleep(1000)
